ea2e7e8f3298dddc8ce83a75ae6db8dc002b99fe
pallav1996 <pnnegi199622@gmail.com>
2019-08-05 15:05:59 +0530

    fixing conflict issues

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ const weather = require('./utils/weather')
 const inp = require('readline-sync')
 
 
-<<<<<<< HEAD
 location.geoCode(inp.question("Enter the place: "), (error, geocodeData) =>{
-=======
-location.geoCode(inp.question("Enter the place: "), (error, data) =>{
->>>>>>> 7733b774a211f68aa8489b0c00afa28ed0ab9a5c
 
     if(error)
         return console.log("Error:", error)
     
-<<<<<<< HEAD
     if(typeof(geocodeData) !== "undefined")
     {
         console.log()
@@ -28,22 +23,6 @@ location.geoCode(inp.question("Enter the place: "), (error, data) =>{
                 //                               bad/not present, func doesn't execute
 
                 console.log("Weather: ", forecastData)
-=======
-    if(typeof(data) !== "undefined")
-    {
-        console.log()
-        for(var item in data)
-        {
-            console.log(item, ":", data[item])
-        }
-
-            weather.forecast(data.latitude, data.longitude, (error, data) =>
-            {
-                //console.log("Error: ", error)  Not needed since if coordinates are
-                //                               bad/not present func doesn't execute
-
-                console.log("Weather: ", data)
->>>>>>> 7733b774a211f68aa8489b0c00afa28ed0ab9a5c
             })
     }
 
--- utils/geocode.js
+++ utils/geocode.js
@@ -8,33 +8,21 @@ const geoCode = (address, callback) =>
     const apiKey = api.geocode    
     const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=' + apiKey;
 
-<<<<<<< HEAD
     request({url: geocodeUrl, json:true}, (error, {body}) =>
 {
-=======
-    request({url: geocodeUrl, json:true}, (error, response) =>
-    {
->>>>>>> 7733b774a211f68aa8489b0c00afa28ed0ab9a5c
     if(error)
     {
         callback("Unable to contact server. Try later", undefined);
     }
 
-<<<<<<< HEAD
     else if(body.features.length === 0)
     {
         // callback("Give proper address", undefined)
         callback("Give proper address", undefined)
-=======
-    else if(response.body.features.length === 0)
-    {
-        callback("Give proper arguments", undefined)
->>>>>>> 7733b774a211f68aa8489b0c00afa28ed0ab9a5c
     }
 
     else{
         const data = {
-<<<<<<< HEAD
             latitude: body.features[0].center[1],
             longitude: body.features[0].center[0],
             location: body.features[0].place_name    
@@ -49,20 +37,3 @@ module.exports = {
     geoCode
 }
 
-=======
-            latitude : response.body.features[0].center[1],
-            longitude : response.body.features[0].center[0],
-            location: response.body.features[0].place_name    
-        }
-        
-        callback(undefined, data)
-        }
-    });
-
-}
-
-
-module.exports = {
-    geoCode
-}
->>>>>>> 7733b774a211f68aa8489b0c00afa28ed0ab9a5c
